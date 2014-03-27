da5e2111c53d32498361f95bdfbd649ba6ad835c
Peter Carlton <pmcarlton@mac.com>
2014-03-27 11:32:47 +0900

    Update rss-filter.pl

--- rss-filter.pl
+++ rss-filter.pl
@@ -3,10 +3,10 @@ $|=1;   #set autoflush
 use utf8; binmode STDOUT, ":utf8"; #needed for Unicode
 
 #preprocess the input stream first
-my $w=chr(0xc2).chr(0xa0); #this is a weird space character that you get sometimes in pubmed!
+my $w=chr(0xc2).chr(0xa0); #this is a Unicode non-breaking space (&nbsp;) that you get sometimes in pubmed!
 my $tlen=85; #how many characters of title to print out at maximum
 while(<STDIN>) {
-s/($w)/ /; #replace the weird space with a real space
+s/($w)/ /; #replace the &nbsp; with a real space
 s/PLoS/PLOS/; #all-uppercase now
 
 my @a=split("__",$_); #feedstail separated each field with "__", now we'll process each field at a time
