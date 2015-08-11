5acf76a43330a48bc924ba3a4c5e9be495f8b087
pmcarlton <pmcarlton@mac.com>
2015-08-11 06:48:28 +0000

    added parsing of bioRxiv feeds

---
+++ biorxiv-filter.pl
@@ -0,0 +1,33 @@
+use strict;
+$|=1;   #set autoflush
+use utf8; 
+binmode STDIN, ":utf8"; #needed for Unicode
+binmode STDOUT, ":utf8"; #needed for Unicode
+
+#preprocess the input stream first
+#my $w=chr(0xc2).chr(0xa0); #this is a weird space character that you get sometimes in pubmed!
+my $tlen=85; #how many characters of title to print out
+while(<STDIN>) {
+my $prg=0;
+#s/($w)/ /; #replace the weird space with a real space
+s/PLoS/PLOS/; #all-uppercase now
+$prg=1 if(/5hmC/ || /Tet1/i || /Tet2/i || /Tet3/i || /hydroxymethylcytosine/i);
+$prg=2 if(/meiosis/i || /synaptonemal/i || /meiot/i); 
+my @a=split("__",$_); #feedstail separated each field with "__", now we'll process each field at a time
+my @tt=split(" ",$a[0]);
+my $t=join(" ",splice(@tt,2));
+$a[2]=~s/\.//g;
+my $rb=$a[2]." ".$a[3];
+my $tl=length($rb);
+if(length($t)>(130-$tl)){
+    $t=substr($t,0,133-$tl);
+    $t=~s/ $//;
+    $t=$t."…";
+    }
+my $out=$t."|".$rb."\n";
+$out=~s/(\W)…/$1/;chomp($out);
+my $cmd="";
+if($prg==1) {$cmd="echo ".qw(').$out.qw(')." | /home/pcarlton/code/5hmctweet.sh \n";}
+if($prg==2) {$cmd="echo ".qw(').$out.qw(')." | /home/pcarlton/code/meiotweet.sh \n";} 
+if($cmd){system($cmd);}
+}
