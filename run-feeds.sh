60026d665b350409f795db2a6adcb855a362a72d
Peter Carlton <pmcarlton@mac.com>
2016-06-04 09:23:32 +0000

    changed to adapt to biorχiv's RSS format

--- run-feeds.sh
+++ run-feeds.sh
@@ -2,4 +2,4 @@ nohup feedstail -i 21600 -f {title}__{tags}__{author}__{link} -r -u 'http://euti
 
 nohup feedstail -i 21600 -f {title}__{tags}__{author}__{link} -r -u 'http://www.ncbi.nlm.nih.gov/entrez/eutils/erss.cgi?rss_guid=1lS0N-smva-GZXzUBm7wiyXZpf_XKDL8e-APbwxwudglrkI9Pw' | perl /home/pcarlton/code/pubmed-rss-twitter/rss-filter.pl | /home/pcarlton/code/5hmctweet.sh &
 
-nohup feedstail -i 21600 -e -r -u "http://connect.biorxiv.org/biorxiv_xml.php?subject=all" -f {title}__{tags}__{author}__{link}__{summary} | perl /home/pcarlton/code/pubmed-rss-twitter/biorxiv-filter.pl &
+nohup feedstail -i 21600 -e -r -u "http://connect.biorxiv.org/biorxiv_xml.php?subject=all" -f {title}__{author}__{link}__{summary} | perl /home/pcarlton/code/pubmed-rss-twitter/biorxiv-filter.pl &
