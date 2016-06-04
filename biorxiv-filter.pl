aa5a78e5835ca5b3b4771fd520851045f99bb134
Peter Carlton <pmcarlton@mac.com>
2016-06-04 09:14:00 +0000

    Updated biorxiv-filter.pl to match changes in biorχiv's own RSS

--- biorxiv-filter.pl
+++ biorxiv-filter.pl
@@ -15,9 +15,11 @@ $prg=1 if(/5hmC/ || /Tet1/i || /Tet2/i || /Tet3/i || /hydroxymethylcytosine/i);
 $prg=2 if(/meiosis/i || /synaptonemal/i || /meiot/i); 
 my @a=split("__",$_); #feedstail separated each field with "__", now we'll process each field at a time
 my @tt=split(" ",$a[0]);
-my $t=join(" ",splice(@tt,2));
-$a[2]=~s/\.//g;
-my $rb=$a[2]." ".$a[3];
+#my $t=join(" ",splice(@tt,2));
+my $t=$a[0];
+$a[1]=~s/\.//g;
+$a[2]=~s/.rss=1//;
+my $rb=$a[1]." ".$a[2];
 my $tl=length($rb);
 if(length($t)>(130-$tl)){
     $t=substr($t,0,133-$tl);
@@ -30,4 +32,8 @@ my $cmd="";
 if($prg==1) {$cmd="echo ".qw(').$out.qw(')." | /home/pcarlton/code/5hmctweet.sh \n";}
 if($prg==2) {$cmd="echo ".qw(').$out.qw(')." | /home/pcarlton/code/meiotweet.sh \n";} 
 if($cmd){system($cmd);}
+#if($prg==2) {print $cmd;}
 }
+
+
+#nohup feedstail -i 21600 -e -r -u "http://connect.biorxiv.org/biorxiv_xml.php?subject=all" -f {title}__{author}__{link}__{summary} | perl /home/pcarlton/code/pubmed-rss-twitter/biorxiv-filter.pl &
